1ba056f897af4f57018b755568a66217770698c5
Thien Nguyen <s3426515@rmit.edu.vn>
2022-12-01 14:50:36 +1100

    Enable broadcasting via websockets

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('/v1')->group(function() {
+    Broadcast::routes(['middleware' => ['auth:sanctum']]);
 
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register']);
