2d49018e9bbd38643e4cf5725b9b5cef68120b4b
Thien Nguyen <s3426515@rmit.edu.vn>
2023-01-17 16:41:56 +1100

    Redirect home page to my portfolio site

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->away('https://tim-nguyen.dev');
 });
 
 Route::get('/dashboard', function () {
